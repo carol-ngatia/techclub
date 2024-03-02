97e74c19557e6a2c8d32df73c3e534d7accee3f2
carol-ngatia <caroljoe716@gmail.com>
2024-03-02 12:18:49 +0300

    third commit

--- BASIC/index.js
+++ BASIC/index.js
@@ -1,3 +1,6 @@
 const component =  document.getElementById('app');
 component. innerHTML =<h1>this is js</h1>
 let weather = "sunny"
+if (weather.toLocaleLowerCase() ==="sunny") {
+    console.log('wear something light')
+}
